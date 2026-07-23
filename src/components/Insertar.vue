b99818bff254160893c7a6b54f075be1388248d5
Rgj <17301662@uttt.edu.mx>
2026-07-22 20:17:34 -0600

    Conectar frontend con la API de .NET en Render

--- src/components/Insertar.vue
+++ src/components/Insertar.vue
@@ -2,7 +2,7 @@
 import { ref } from 'vue'
 import axios from 'axios'
 
-const API_URL = 'http://localhost:3000/api/products'
+const API_URL = 'https://eshop-services-az0q.onrender.com'
 
 const form = ref({
   name: '',
